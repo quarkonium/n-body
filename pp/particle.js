23d857bb2fef2534a8681abd1d5d291479a3ed8f
quarkonum <dhatton@gmx.ch>
2011-02-19 16:06:34 +0100

    first commit

---
+++ pp/particle.js
@@ -0,0 +1,21 @@
+//Copyright 2011 quarkonium http://www.quarkonium.com 
+//Released under the MIT License
+
+var Partice =
+{
+  mass: -1, 
+  position: [-1, -1, -1], //Evolves frame to frame
+  velocity: [-1, -1, -1], //Evolves frame to frame
+  force:    [-1, -1, -1], //Computed for each frame
+  update: function(t)
+  {
+    
+  },
+  draw: function()
+  {
+  },
+  force: function(f)
+  {
+    //Sum the forces
+  }
+};
