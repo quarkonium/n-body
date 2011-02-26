f00baa2b418a44898e1718aeae8509bfcf4498af
quarkonum <dhatton@gmx.ch>
2011-02-26 21:39:03 +0100

    Modifications

--- pp/js/rk4integrator.js
+++ pp/js/rk4integrator.js
@@ -3,26 +3,4 @@
 
 function RK4Integrator() =
 {
-  this.mass: -1; 
-  this.position: [-1, -1, -1]; //Evolves frame to frame
-  this.velocity: [-1, -1, -1]; //Evolves frame to frame
-  this.force:    [-1, -1, -1]; //Computed for each frame
-
-  this.update = function(t)
-  {
-    
-  };
-
-  this.draw: function()
-  {
-  };
-
-  this.addForce: function(f)
-  {
-    //Sum the forces
-    for(i=0; i<3; i++)
-    {
-      this.force[i]+=f[i];
-    }
-  }
 };
